126714906571d00b28ed26d593b44bd43ea3b357
KindaExists <ehrencastillo@gmail.com>
2021-08-21 14:04:08 +0800

    added function to update html board

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 
 const GameBoard = (() => {
     gameBoard = [
-        ['X', 'X', 'X'],
-        ['X', 'X', 'O'],
-        ['O', 'O', 'O']
+        'X', 'X', 'X',
+        'X', 'X', 'O',
+        'O', 'O', 'O'
     ];
 
     return {
@@ -21,3 +21,17 @@ const PlayerFactory = (symbol) => {
 const player1 = PlayerFactory('X');
 console.log(player1.symbol);
 console.log(GameBoard.gameBoard);
+
+
+
+const displayBoard = document.querySelector('#board');
+const displayTiles = displayBoard.querySelectorAll('.board-tile');
+
+function updateDisplayBoard() {
+    const gameBoard = GameBoard.gameBoard;
+    for(let i = 0; i < gameBoard.length; i++){
+        displayTiles[i].textContent = gameBoard[i];
+    }
+}
+
+updateDisplayBoard();
