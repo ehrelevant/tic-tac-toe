856caa7ff59f1f68806c6094d4eedbe3ac598ece
KindaExists <ehrencastillo@gmail.com>
2021-08-22 13:30:49 +0800

    put render in displayController Module

--- app.js
+++ app.js
@@ -1,50 +1,43 @@
 
 const GameBoard = (() => {
-    gameBoard = [
-        'O', 'O', 'O',
-        'O', 'O', 'O',
-        'O', 'O', 'O'
+    let board = [
+        '', '', '',
+        '', '', '',
+        '', '', ''
     ];
+    let turn = 0;
 
     return {
-        gameBoard
+        board
     };
 })();
+
+
+const DisplayController = (() => {
+    function renderBoard(board) {
+        for(let i = 0; i < board.length; i++){
+            displayTiles[i].textContent = board[i];
+        }
+    }
+
+    return {
+        renderBoard
+    };
+})();
+
 
 const PlayerFactory = (symbol) => {
     return {
         symbol
     };
-}
+};
+
 
 
 const player1 = PlayerFactory('X');
-console.log(player1.symbol);
-console.log(GameBoard.gameBoard);
-
-
+const player2 = PlayerFactory('O');
 
 const displayBoard = document.querySelector('#board');
 const displayTiles = displayBoard.querySelectorAll('.board-tile');
 
-function updateDisplayBoard() {
-    const gameBoard = GameBoard.gameBoard;
-    for(let i = 0; i < gameBoard.length; i++){
-        displayTiles[i].textContent = gameBoard[i];
-    }
-}
-
-displayBoard.addEventListener('click', markTile, true)
-
-function markTile(e) {
-    const target = e.target;
-    if (target.className == 'board-tile') {
-        const ind = target.dataset['ind'];
-        console.log(ind);
-        GameBoard.gameBoard[ind] = 'X';
-        updateDisplayBoard()
-    }
-}
-
-
-updateDisplayBoard();
+DisplayController.renderBoard(GameBoard.board);
